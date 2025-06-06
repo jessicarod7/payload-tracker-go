c57e8e4da895afdf8aa102226e3a2146db935368
dehort <dehort@redhat.com>
2025-06-06 16:51:27 -0500

    Revert "Add some print statements to see the caching working (#300)" (#301)
    This reverts commit be0eab446ce998e54450203191bab0e7e9e35447.

--- internal/queries/queries_consumer.go
+++ internal/queries/queries_consumer.go
@@ -57,7 +57,6 @@ func (p *PayloadFieldsRepositoryFromDB) GetSource(sourceName string) models.Sour
 func (p *PayloadFieldsRepositoryFromCache) GetStatus(statusName string) models.Statuses {
 	cached, ok := p.statusCache.Get(statusName)
 	if ok {
-		fmt.Printf("status cache hit: %+v\n", cached)
 		return cached
 	}
 
@@ -65,15 +64,12 @@ func (p *PayloadFieldsRepositoryFromCache) GetStatus(statusName string) models.S
 
 	p.statusCache.Add(statusName, dbEntry)
 
-	fmt.Printf("status cache miss: %+v\n", dbEntry)
-
 	return dbEntry
 }
 
 func (p *PayloadFieldsRepositoryFromCache) GetService(serviceName string) models.Services {
 	cached, ok := p.serviceCache.Get(serviceName)
 	if ok {
-		fmt.Printf("service cache hit: %+v\n", cached)
 		return cached
 	}
 
@@ -81,23 +77,18 @@ func (p *PayloadFieldsRepositoryFromCache) GetService(serviceName string) models
 
 	p.serviceCache.Add(serviceName, dbEntry)
 
-	fmt.Printf("service cache miss: %+v\n", dbEntry)
-
 	return dbEntry
 }
 
 func (p *PayloadFieldsRepositoryFromCache) GetSource(sourceName string) models.Sources {
 	cached, ok := p.sourceCache.Get(sourceName)
 	if ok {
-		fmt.Printf("source cache hit: %+v\n", cached)
 		return cached
 	}
 
 	dbEntry := p.PayloadFields.GetSource(sourceName)
 
 	p.sourceCache.Add(sourceName, dbEntry)
-
-	fmt.Printf("source cache miss: %+v\n", dbEntry)
 
 	return dbEntry
 }
